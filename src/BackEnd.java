bc27bffcc23e2853b93004c5c9beb302db18b0da
jimmyd123 <aggiebaseball2020@gmail.com>
2015-04-07 10:08:53 -0500

    FinishBackend

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,4 +1,29 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 
 public class BackEnd {
+	
+	public Map<String, List<String>> data;
+	data = new TreeMap<String, List<String>>();
+	
+	public List<String> getMessage(String username)
+	{
+		return data.get(username);
+		
+	}
+	
+	public void addMessage(String username, String msg)
+	{
+		data.put(username, msg);
+	}
+	
+	public List<String> getUsers()
+	{
+		
+		return Arrays.asList(data.keySet().toArray());
+		
+	}
 
 }
